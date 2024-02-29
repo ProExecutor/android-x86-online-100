14994dfaf8e2a4071cb71c199e78bc03624c4780
ProExecutor <123886692+ProExecutor@users.noreply.github.com>
2024-03-01 00:03:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,9 @@
-from flask import Flask, render_template_string
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    html_content = """
-    <!DOCTYPE HTML>
-    <html>
-    <head>
-      <style>
-        .center {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-        }
-      </style>
-    </head>
-    <body>
-      <script src="jsvm.js"></script>
-      <script>
-        javascript:(function () {     
-          var script =  document.createElement('script');    
-          script.src="//cdn.jsdelivr.net/npm/eruda";     
-          document.body.appendChild(script);    
-          script.onload = function () {         
-            eruda.init()     
-          } 
-        })();
-      </script>
-      <vm-instance id="vm" class="center"></vm-instance>
-      <script>
-        document.getElementById('vm').setAttribute('cd-rom', 'cdrom/android-x86-1.6-r2.iso');
-      </script>
-    </body>
-    </html>
-    """
-    return render_template_string(html_content)
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
